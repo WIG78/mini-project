44e68d188b0a910555eefb8a19dfd60f32869d59
William Marvin <williammarvin.pro@gmail.com>
2021-01-11 13:38:35 +0100

    materialize mis en place

--- src/main/java/progWeb/controller/MyController.java
+++ src/main/java/progWeb/controller/MyController.java
@@ -79,7 +79,7 @@ public class MyController {
 			response.addCookie(new Cookie("nbHits", "" + chosen.getNbHits()));
 			nextFoe(request, response);
 		}
-		response.sendRedirect("/showCharacter.html");
+		response.sendRedirect("/index.html");
 	}
 
 //	@RequestMapping(value = "/character", method = RequestMethod.GET)
@@ -133,6 +133,63 @@ public class MyController {
 		response.sendRedirect("/showCharacter.html");
 	}
 
+	@RequestMapping(value = "/Attack", method = RequestMethod.GET)
+	public void attack(HttpServletRequest request, HttpServletResponse response)
+			throws UnsupportedEncodingException, IOException {
+
+		String damStr = request.getParameter("dammage");
+		System.out.println(damStr);
+		if (damStr != null) {
+			// Valeur de l'attaque lancé contre foe
+			int dammage = Integer.parseInt(damStr);
+
+			// Stats perso
+			int hp = 0;
+			int nbHits = 0;
+			int hpMax = 0;
+			int dodge = 0;
+			// Stats foe
+			int foeHP = 0;
+			int foeAttack = 0;
+
+			for (Cookie c : request.getCookies()) {
+
+				if (c.getName().equals("HP")) {
+					hp = Integer.parseInt(c.getValue());
+				}
+				if (c.getName().equals("HPMax")) {
+					hpMax = Integer.parseInt(c.getValue());
+				}
+				if (c.getName().equals("nbHits")) {
+					nbHits = Integer.parseInt(c.getValue());
+				}
+				if (c.getName().equals("dodge")) {
+					dodge = Integer.parseInt(c.getValue());
+				}
+
+				if (c.getName().equals("foeHP")) {
+					foeHP = Integer.parseInt(c.getValue());
+				}
+				if (c.getName().equals("foeAttack")) {
+					foeAttack = Integer.parseInt(c.getValue());
+				}
+			}
+
+
+			double random = Math.random();
+			if(random > dodge){
+
+			}
+			hp = (hp - foeAttack > 0) ? hp - foeAttack : 0;
+			// Nombre de dégats reçu pour le personnage
+			nbHits = (nbHits + foeAttack > hpMax) ? hpMax : nbHits + foeAttack;
+
+			response.addCookie(new Cookie("HP", "" + hp));
+			response.addCookie(new Cookie("nbHits", "" + nbHits));
+		}
+		response.sendRedirect("/showCharacter.html");
+	}
+
 	@RequestMapping(value = "/nextFoe", method = RequestMethod.GET)
 	public void nextFoe(HttpServletRequest request, HttpServletResponse response)
 			throws UnsupportedEncodingException, IOException {
@@ -148,6 +205,7 @@ public class MyController {
 			response.addCookie(new Cookie("foeName", foe.getName()));
 			response.addCookie(new Cookie("foeHP", "" + foe.getHpMax()));
 			response.addCookie(new Cookie("foeAttack", "" + foe.getAttack()));
+			response.addCookie(new Cookie("foeDodge", "" + foe.getDodgeProbability()));
 		} catch (IndexOutOfBoundsException e) {
 			response.getOutputStream().write("Tous les ennemis sont vaincus".getBytes("UTF-8"));
 		}
